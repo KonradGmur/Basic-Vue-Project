4b551503b9a19d7b98f2a5233228216999eb768d
Konrad <konrad.gmur@gmail.com>
2020-10-18 12:10:17 +0200

    Adding AppLayout.vue to project

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-layout">
+    <header class="header">
+      <div>
+        <img class="img" src="../style/assets/logo.svg" alt="logo" />
+        <div>Centrum wsparcia firmy Gmur Corporation</div>
+      </div>
+    </header>
+
+    <router-view />
+  </div>
+</template>
+
+<style lang="stylus">
+@import '../style/style/main';
+</style>
+
+<style lang="stylus" scoped>
+.header {
+  .img {
+    width: 64px;
+    height: 64px;
+  }
+}
+</style>
