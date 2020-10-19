bb44bd770a772ff361c20c5b339cda9923ae5540
Konrad <konrad.gmur@gmail.com>
2020-10-19 20:33:26 +0200

    Adding paths in router.js

--- src/router.js
+++ src/router.js
@@ -1,4 +1,11 @@
 import Vue from "Vue";
 import VueRouter from "vue-router";
+import Home from "./components/Home.vue";
+import FAQ from "./components/FAQ.vue";
 
 Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", name: "home", component: Home },
+  { path: "/faq", name: "faq", component: FAQ },
+];
