9417cf8eb8fcec16facfab274c45e02f18d04483
Konrad <konrad.gmur@gmail.com>
2020-10-19 20:32:48 +0200

    Adding Home.vue to project

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="home">
+    <h1>Witaj w naszym centrum wsparcia</h1>
+    <p>
+      Jesteśmy tu aby pomóc. Proszę przeczytaj najpierw nasze najczęściej
+      zadawane pytania. Jeśli nie znalazłeś odpowiedzi na swoje pytanie,
+      <a href=""> wyślij je do nas</a>
+    </p>
+  </main>
+</template>
