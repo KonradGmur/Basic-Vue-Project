d9c04163ae81c67c35adb9ce896301278afbcdc2
Konrad <konrad.gmur@gmail.com>
2020-10-28 18:55:44 +0100

    Adding a loader to the application

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -2,6 +2,9 @@
   <main class="faq">
     <h1>Najczęściej zadawane pytania</h1>
     <div class="error" v-if="error">Problem z wczytaniem pytań</div>
+
+    <Loading v-if="loading" />
+
     <section class="list">
       <article v-for="question of questions">
         <h2 v-html="question.title"></h2>
@@ -17,23 +20,39 @@ export default {
     return {
       questions: [],
       error: null,
+      loading: false,
     };
   },
-  created() {
-    fetch("http://localhost:3000/questions")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          return Promise.reject("error");
-        }
-      })
-      .then((result) => {
-        this.questions = result;
-      })
-      .catch((e) => {
-        this.error = e;
-      });
+  // created() {
+  //   fetch("http://localhost:3000/questions")
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       } else {
+  //         return Promise.reject("error");
+  //       }
+  //     })
+  //     .then((result) => {
+  //       this.questions = result;
+  //     })
+  //     .catch((e) => {
+  //       this.error = e;
+  //     });
+  // },
+
+  async created() {
+    this.loading = true;
+    try {
+      const response = await fetch("http://localhost:3000/questions");
+      if (response.ok) {
+        this.questions = await response.json;
+      } else {
+        throw new Error("error");
+      }
+    } catch (e) {
+      this.error = e;
+    }
+    this.loading = false;
   },
 };
 </script>
