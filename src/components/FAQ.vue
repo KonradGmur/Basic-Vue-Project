c347585703c915229ef6fad8bf5c5a5fe1c33a56
Konrad <konrad.gmur@gmail.com>
2020-10-28 18:36:13 +0100

    Downloading data from the server part 1

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,5 +1,23 @@
 <template>
   <main class="faq">
     <h1>Najczęściej zadawane pytania</h1>
+    <div class="error" v-if="error">Problem z wczytaniem pytań</div>
+    <section class="list">
+      <article v-for="question of questions">
+        <h2 v-html="question.title"></h2>
+        <p v-html="question.content"></p>
+      </article>
+    </section>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      questions: [],
+      error: null,
+    };
+  },
+};
+</script>
