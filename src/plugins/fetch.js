13a59fdbb6867f24f15d2102911f478fea6ae2d6
Konrad <konrad.gmur@gmail.com>
2020-10-31 17:22:52 +0100

    Adding mixins downloading data part 2

--- src/plugins/fetch.js
+++ src/plugins/fetch.js
@@ -1,8 +1,21 @@
 let baseUrl;
 
+export async function $fetch(url) {
+  const response = await fetch(`${baseUrl}$(url)`);
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    const error = new Error("error");
+    throw error;
+  }
+}
+
 export default {
-  install(options) {
+  install(Vue, options) {
     console.log("Plugin installed", options);
     baseUrl = options.baseUrl;
+
+    Vue.prototype.$fetch = $fetch;
   },
 };
