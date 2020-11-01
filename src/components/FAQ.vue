66d5ac5170e87fa71a9e2f416a03a1fbf763d71b
Konrad <konrad.gmur@gmail.com>
2020-11-01 15:37:23 +0100

    update FAQ.vue

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="faq">
     <h1>Najczęściej zadawane pytania</h1>
-    <div class="error" v-if="error">Problem z wczytaniem pytań</div>
+    <div class="error" v-if="hasRemoteErrors">Problem z wczytaniem pytań</div>
 
     <Loading v-if="remoteDataBusy" />
 
@@ -22,43 +22,5 @@ export default {
       questionList: "questions",
     }),
   ],
-  data() {
-    return {
-      questions: [],
-      error: null,
-      loading: false,
-    };
-  },
-  // created() {
-  //   fetch("http://localhost:3000/questions")
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       } else {
-  //         return Promise.reject("error");
-  //       }
-  //     })
-  //     .then((result) => {
-  //       this.questions = result;
-  //     })
-  //     .catch((e) => {
-  //       this.error = e;
-  //     });
-  // },
-
-  async created() {
-    this.loading = true;
-    try {
-      const response = await fetch("http://localhost:3000/questions");
-      if (response.ok) {
-        this.questions = await response.json;
-      } else {
-        throw new Error("error");
-      }
-    } catch (e) {
-      this.error = e;
-    }
-    this.loading = false;
-  },
 };
 </script>
