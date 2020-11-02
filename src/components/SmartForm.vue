2ca9499881358f7a7433101586866a76f21e2026
Konrad <konrad.gmur@gmail.com>
2020-11-02 20:54:23 +0100

    Adding first form to project part 4

--- src/components/SmartForm.vue
+++ src/components/SmartForm.vue
@@ -16,3 +16,42 @@
     </transition>
   </form>
 </template>
+
+<script>
+export default {
+  methods: {
+    async submit() {
+      if (this.valid && !this.busy) {
+        this.error = null;
+        this.busy = true;
+        try {
+          await this.operation();
+        } catch (e) {
+          this.error = e.message;
+        }
+        this.busy - false;
+      }
+    },
+  },
+  data() {
+    return {
+      error: null,
+      busy: false,
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    operation: {
+      type: Function,
+      required: true,
+    },
+    valid: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
