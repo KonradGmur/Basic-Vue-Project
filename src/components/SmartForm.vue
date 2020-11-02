16044d0e98c668b75d5a1dc5999292cc3f69e79e
Konrad <konrad.gmur@gmail.com>
2020-11-02 20:41:40 +0100

    Adding first form to project part 3

--- src/components/SmartForm.vue
+++ src/components/SmartForm.vue
@@ -2,6 +2,17 @@
   <form @submit.prevent="submit">
     <section class="content">
       <h2>{{ title }}</h2>
+      <slot />
+
+      <div class="actions">
+        <slot name="actions" />
+      </div>
+
+      <div class="error" v-if="error">{{ error }}</div>
     </section>
+
+    <transition name="fade">
+      <Loading v-if="busy" class="overlay" />
+    </transition>
   </form>
 </template>
