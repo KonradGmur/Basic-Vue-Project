a01a94c8c98a7d98870de6ab6d9ee4505367dcc6
Konrad <konrad.gmur@gmail.com>
2020-11-03 20:17:27 +0100

    Adding first form part 1

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="login">
+    <h1>Zaloguj się aby kontynuować</h1>
+    <form>
+      <FormInput
+        name="username"
+        :value="username"
+        placeholder="Nazwa uzytkownika"
+      />
+    </form>
+  </main>
+</template>
