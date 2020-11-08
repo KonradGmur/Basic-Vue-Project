0bee6ac8a0b74015df5ec2ba7a165b678946b1dc
Konrad Gmur <you@example.comegit config --global user.email konrad.gmur@gmail.com  git config --global user.name Konrad>
2020-11-08 13:44:31 +0100

    update NavMenu.vue

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,6 +2,7 @@
     <nav class="menu">
         <router-link :to="{name:'home'}" exact>Strona Główna</router-link>,
         <router-link :to="{name:'faq'} ">FAQ</router-link>,
+        <router-link :to ="{name: 'login'}">Login</router-link>
     <nav>
 </template>
 
