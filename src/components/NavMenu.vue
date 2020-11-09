7fca3a0c50b75cd0bb76eb3c1cb300d46a269189
Konrad <konrad.gmur@gmail.com>
2020-11-09 20:26:07 +0100

    Support for logging in and logging out part 3

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -3,9 +3,28 @@
         <router-link :to="{name:'home'}" exact>Strona Główna</router-link>,
         <router-link :to="{name:'faq'} ">FAQ</router-link>,
         <div class="spacer"></div>
-        <router-link :to ="{name: 'login'}">Login</router-link>
+
+        <template v-if="$state.user">
+          <a>{{$state.user.username}}</a>
+          <a @click="logout">Wyloguj się</a>
+        </template>
+
+        <router-link v-else :to ="{name: 'login'}">Login</router-link>
     <nav>
 </template>
+
+<script>
+export default {
+  methods:{
+    async logout(){
+      const result = await this.$fetch('logout')
+      if(result.status === 'ok'){
+        this.$state.user = null
+      }
+    }
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 @import '../style/imports';
