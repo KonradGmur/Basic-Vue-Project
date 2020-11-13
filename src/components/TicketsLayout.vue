9d2bc8758c170c1f08413dfec94b2ace55d93255
Konrad <konrad.gmur@gmail.com>
2020-11-13 20:35:12 +0100

    Adding tickets

--- src/components/TicketsLayout.vue
+++ src/components/TicketsLayout.vue
@@ -1,5 +1,15 @@
 <template>
   <main class="tickets-layout">
     <h1>Zgłoszenia klientów</h1>
+    <Ticket />
   </main>
 </template>
+
+<script>
+import Ticket from "./Ticket";
+export default {
+  components: {
+    Ticket,
+  },
+};
+</script>
