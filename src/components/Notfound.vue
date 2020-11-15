02ef3613ef167dfcb28d93c05632f69d3116df13
Konrad <konrad.gmur@gmail.com>
2020-11-15 15:10:53 +0100

    Adding a 404 page

---
+++ src/components/Notfound.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="not-found">
+    <h1>Nie znaleziono strony</h1>
+    <p class="more-info">
+      Przepraszamy, ale nie mogliśmy znaleźć strony ,której szukasz. Jeśli
+      chcesz naciśnij poniższy przycisk aby wrócić do strony głównej.
+    </p>
+    <div class="actions">
+      <router-link tag="button" :to="{ name: 'home' }"
+        >Powrót do strony głównej</router-link
+      >
+    </div>
+  </main>
+</template>
+
+<style lang="stylus" scoped>
+.more-info{
+    text-align:center
+}
+</style>
