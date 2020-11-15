df28ad89595cf7a981c58e48bc10ffa920acad31
Konrad <konrad.gmur@gmail.com>
2020-11-15 13:43:47 +0100

    adding a view displaying notifications part 1

---
+++ src/components/TicketVue.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ticket">
+    <h2>Zgłoszenie</h2>
+    <Loading v-if="remoteDataBusy" />
+
+    <div class="empty" v-else-if="!ticket">Zgłoszenie nie znalezione</div>
+
+    <template v-else>
+      <section class="infos">
+        <div class="info">
+          Utworzono <strong>{{ ticket.date | date }}</strong>
+        </div>
+        <div class="info">
+          Autor: <strong>{{ ticket.user.unsername }}</strong>
+        </div>
+        <div class="info">
+          Status: <span class="badge">{{ ticket.status }}</span>
+        </div>
+      </section>
+      <section class="content">
+        <h3>{{ ticket.title }}</h3>
+        <p>{{ ticket.descripion }}</p>
+      </section>
+    </template>
+  </div>
+</template>
