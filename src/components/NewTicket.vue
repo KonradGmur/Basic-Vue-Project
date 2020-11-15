4070b5862a6284294dcd439a8e28999145a74e83
Konrad <konrad.gmur@gmail.com>
2020-11-15 12:58:45 +0100

    Adding a form for adding notifications

--- src/components/NewTicket.vue
+++ src/components/NewTicket.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="new-ticket">
     <h1>Nowe zgłoszenie</h1>
+    <SmartForm title="Nowe zgłoszenie" :oparation="operation" :valid="valid">
+      <FormInput
+        name="title"
+        v-model="title"
+        placeholder="Krótki opis (max 100 znaków)"
+        maxlenght="100"
+        required
+      />
+    </SmartForm>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      description: "",
+    };
+  },
+  computed: {
+    valid() {
+      return !!this.title && !!title.description;
+    },
+  },
+  methods: {
+    async operation() {},
+  },
+};
+</script>
