61830dec60d6b062dbb36deb904f5d3fc11818dd
Konrad <konrad.gmur@gmail.com>
2020-11-15 12:39:35 +0100

    Update TicketLayout.vue

--- src/components/TicketsLayout.vue
+++ src/components/TicketsLayout.vue
@@ -1,15 +1,21 @@
 <template>
   <main class="tickets-layout">
     <h1>Zgłoszenia klientów</h1>
-    <Ticket />
+    <div class="actions">
+      <router-link
+        v-if="$route.name !== 'tickets'"
+        tag="button"
+        class="secondary"
+        :to="{ name: 'tickets' }"
+        >Zobacz wszystkie zgłoszenia</router-link
+      >
+      <router-link
+        v-if="$route.name !== 'new-ticket'"
+        tag="button"
+        :to="{ name: 'new-ticket' }"
+        >Nowe zgłoszenie</router-link
+      >
+    </div>
+    <router-view />
   </main>
 </template>
-
-<script>
-import Ticket from "./Ticket";
-export default {
-  components: {
-    Ticket,
-  },
-};
-</script>
